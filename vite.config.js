bb08cc1fa2bfcc6c11cc10ea63aea874258523b2
klu2300032850 <2300032850@kluniversity.in>
2026-05-03 11:53:30 +0530

    added gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,17 @@
+<<<<<<< HEAD
 import { defineConfig } from 'vite' import { resolve, dirname } from 'path' import { fileURLToPath } from 'url' const __dirname = dirname(fileURLToPath(import.meta.url)) export default defineConfig({ build: { rollupOptions: { input: { main: resolve(__dirname, 'index.html'), celebrations: resolve(__dirname, 'celebrations.html'), }, }, }, })
+=======
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        celebrations: resolve(__dirname, 'celebrations.html'),
+      },
+    },
+  },
+})
+>>>>>>> c38dcb1 (second commit)
